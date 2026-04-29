d0a0d1aaa78bf472031a2b44abffc7c9b533e1b0
IGATAJOHN <igatusjohn15@gmail.com>
2026-04-29 04:34:07 +0100

    chore: rename api to server for Vercel deployment

--- db/seed.ts
+++ db/seed.ts
@@ -1,4 +1,4 @@
-import { getDb } from "../api/queries/connection";
+import { getDb } from "../server/queries/connection";
 import { tests, testimonials, settings } from "./schema";
 
 async function seed() {
